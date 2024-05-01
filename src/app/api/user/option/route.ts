7fd33a1b8521e17b736f7c47e7e8363bf6951c55
Rishav Medhi <rishav164@gmail.com>
2024-05-01 20:08:07 +0530

    user option api - removing question id as query param and adding qid extracting logic

--- src/app/api/user/option/route.ts
+++ src/app/api/user/option/route.ts
@@ -1,8 +1,10 @@
+import { fetchGameQuestionsByGameId } from "@/repo/gameQuestionMapping.repo";
+import { fetchGameStatus } from "@/repo/gameStatus.repo";
 import { findUserByDeviceId } from "@/repo/user.repo";
 import { addUserOption } from "@/repo/userOptions.repo";
 import { reviewGameSituation } from "@/services/backend/game.service";
 import { headers } from "next/headers";
-export async function POST(request: Request, { params }: { params: { qid: number } }) {
+export async function POST(request: Request) {
   try {
     const res = await request.json();
     const headersList = headers();
@@ -19,14 +21,26 @@ export async function POST(request: Request, { params }: { params: { qid: number
 
     // fetch userId from deviceId
     const userData = await findUserByDeviceId(deviceId!);
+    console.log(userData)
     if(!userData || userData.length===0){
       return Response.json({
         message: "User not found",
         error: true,
       }); 
     }
+
+    // return game status
+    const gameStatus = await fetchGameStatus(res.gameId);
+    console.log("gameStatus", gameStatus);
+    
+    // fetch question details from game question mapping
+    const gameQuestionMapping = await fetchGameQuestionsByGameId(res.gameId);
+    console.log(gameQuestionMapping);
+    const roundQid = gameQuestionMapping[gameStatus[0].round];
+    // const questionData = await fetchQ(roundQid.question_id!);
+
     // save option to db
-    const userOption = await addUserOption(res.userOption, userData[0].id, res.gameId, params.qid);
+    const userOption = await addUserOption(res.userOption, userData[0].id, res.gameId, roundQid.question_id!);
     // send request to game service to review if there are any changes to the status
     reviewGameSituation(res.gameId)
 
