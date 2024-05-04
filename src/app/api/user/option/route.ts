1e1920e9ec3251561dd369858c91fdcb4e2d342d
Rishav Medhi <rishav164@gmail.com>
2024-05-04 12:49:23 +0530

    option filling - before option saving checks: checking game state , checking if entry exists

--- src/app/api/user/option/route.ts
+++ src/app/api/user/option/route.ts
@@ -1,7 +1,7 @@
 import { fetchGameQuestionsByGameId } from "@/repo/gameQuestionMapping.repo";
 import { fetchGameStatus } from "@/repo/gameStatus.repo";
 import { findUserByDeviceId } from "@/repo/user.repo";
-import { addUserOption } from "@/repo/userOptions.repo";
+import { addUserOption, fetchOptionByUser } from "@/repo/userOptions.repo";
 import { reviewGameSituation } from "@/services/backend/game.service";
 import { headers } from "next/headers";
 export async function POST(request: Request) {
@@ -16,12 +16,8 @@ export async function POST(request: Request) {
       });
     }
 
-    // TODO: check if options are entered during option_filling stage
-    // TODO: restrict if option is already entered by the user
-
     // fetch userId from deviceId
     const userData = await findUserByDeviceId(deviceId!);
-    console.log(userData)
     if(!userData || userData.length===0){
       return Response.json({
         message: "User not found",
@@ -31,24 +27,36 @@ export async function POST(request: Request) {
 
     // return game status
     const gameStatus = await fetchGameStatus(res.gameId);
-    console.log("gameStatus", gameStatus);
+    
+    // if option_filling is not the current state and there is an attempt to save option
+    if(gameStatus[0].option_filling===2){
+      return Response.json({
+        error: true,
+        message: "User option not accepted in this game state"
+      })
+    }
     
     // fetch question details from game question mapping
     const gameQuestionMapping = await fetchGameQuestionsByGameId(res.gameId);
-    console.log(gameQuestionMapping);
     const roundQid = gameQuestionMapping[gameStatus[0].round];
-    // const questionData = await fetchQ(roundQid.question_id!);
 
+    // check if option is already inserted in db
+    const userOption = await fetchOptionByUser(userData[0].id, res.gameId, roundQid.question_id!);
+    if(userOption && userOption.length>0){
+      return Response.json({
+        error: true,
+        message: "User option already accepted"
+      })
+    }
     // save option to db
-    const userOption = await addUserOption(res.userOption, userData[0].id, res.gameId, roundQid.question_id!);
+    await addUserOption(res.userOption, userData[0].id, res.gameId, roundQid.question_id!);
     // send request to game service to review if there are any changes to the status
     reviewGameSituation(res.gameId)
 
     return Response.json({
       message: "User option saved successfully",
       error: false,
-    })
-    
+    }) 
   }
   catch(e){
     console.log(e);
